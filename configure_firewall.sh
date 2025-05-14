46c407e9c612276b62754a56a93a44cf7a7d0eb7
marendtLinux <martinarendt@posteo.de>
2025-05-14 17:18:53 +0200

    added functions for docker firewall configuration

--- configure_firewall.sh
+++ configure_firewall.sh
@@ -12,44 +12,74 @@
 
 TESTPORT=80
 
-#array, that stores all unique ip-addresses of logged in users
-#if a user is logged in several times with the same ipaddress, the ip-address is stored only once
-logged_in_unique_ip_addresses=()
 
+function check_if_docker_is_installed () {
 
-function add_ipaddress_to_array_of_logged_in_unique_ip_addresses () { #tested
-
-	local ipaddress_already_exists=FALSE
-	#echo $1
-
-	#check, if ipaddress is already in array
-	for ipaddress in ${logged_in_unique_ip_addresses[*]}
-	do
-	    if [ "$ipaddress" = "$1" ] ; then    	
-		ipaddress_already_exists=TRUE  
-		break
-	    fi
-	done
-
-	#if ipaddress is not in array, store it there
-	if [ "$ipaddress_already_exists" = "FALSE" ]
+	if nft list ruleset | grep DOCKER-USER
 	then
-		#add ipaddress to array
-		logged_in_unique_ip_addresses[${#logged_in_unique_ip_addresses[@]}]="$1"
+		printf "Docker is installed!\n"
 		return 0
-	else 
+	else
+		"Docker is not installed!\n"
 		return 1
 	fi
 }
 
+# $1: ipaddress
+# $2: Port
+function allow_port_access_for_ipaddress_in_docker () {
+	
+	#if the ipaddress is not already granted port access
+	if ! iptables -n -L DOCKER-USER  | grep $2 | grep $1
+	then
+		#allow packets for port $2 and ip-address $1, 
+		# -m conntrack --ctorigdstpo references the external port that is mapped to the docker port
+		iptables -I DOCKER-USER -i $(get_external_network_interface) -m conntrack --ctorigdstpo $2 -s $1 -j ACCEPT
+		
+		printf "port access granted for ipaddress %s port %s\n in DOCKER-USER" $1 $2
+	fi
+}
+
+# $1: ipaddress
+# $2: Port
+function remove_port_access_for_ipaddress_in_docker() {
+
+	#extract line number of rule to remove
+	line_number_rule_to_remove=$(iptables -L DOCKER-USER --line-numbers | grep $1 | grep $2 | awk  '{print $1}') 
+	
+	#remove the rule
+	iptables -D DOCKER-USER $line_number_rule_to_remove
+	
+	if [ $? -eq 0 ]
+	then
+		printf "port access removed for ipaddress %s port %s in DOCKER-USER\n" $1 $2
+	else
+		printf "Error in removing Docker port access for %s %s \n" $1 $2
+	fi
+}
+
+
+# $1: Port
+function insert_drop_all_packets_rule_for_port_in_docker () {
+	
+	iptables -A DOCKER-USER -i $(get_external_network_interface) -m conntrack --ctorigdstpo $1 -j DROP
+}
 
 
 
 # function allow_ipaddress_port_access
 # $1: ipaddress
 # $2: Port
-function allow_port_access_for_ipaddress () { #tested
+function allow_port_access_for_ipaddress () { 
 
+	check_if_docker_is_installed
+	
+	#if docker is installed
+	if [ $? -eq 0 ]
+	then
+		allow_port_access_for_ipaddress_in_docker $1 $2
+	fi
+	
 	#if the ipaddress is not already granted port access
 	if ! nft -a list table inet secure_port_guard | grep "tcp dport $2 ip saddr $1 accept"
 	then
@@ -59,16 +89,24 @@ function allow_port_access_for_ipaddress () { #tested
 		#allow port access for port $2, for ipaddress $1, place the rule before drop all rule 
 		nft insert rule inet secure_port_guard INPUT position $handle_number_for_drop_all_rule tcp dport $2 ip saddr { $1 } accept
 		
-		
 		printf "port access granted for ipaddress %s port %s\n" $1 $2
 	fi
+	
 }
 
 
 # function remove_port_access_for_ipaddress
 # $1: ipaddress
 # $2: Port
-function remove_port_access_for_ipaddress () { #tested
+function remove_port_access_for_ipaddress () { 
+
+	check_if_docker_is_installed
+	
+	#if docker is installed
+	if [ $? -eq 0 ]
+	then
+		remove_port_access_for_ipaddress_in_docker $1 $2
+	fi
 
 	#get the handle number in the nft firewall for the rule, that allows port access for the specified ipaddress
 	handle_number_port_access=$(nft -a list table inet secure_port_guard | grep "tcp dport $2 ip saddr $1 accept" | awk 'NR==1 {print $10}')
@@ -80,13 +118,47 @@ function remove_port_access_for_ipaddress () { #tested
 }
 
 
+# $1: Port
+function insert_drop_all_packets_rule_for_port () {
+
+	#block all other for port
+	nft add rule inet secure_port_guard INPUT tcp dport $1 drop
+
+}
+
+function inital_setup_nft_firewall_table () {
+
+	nft add table inet secure_port_guard
+
+	nft add chain inet secure_port_guard INPUT { type filter hook input priority 0 \; policy accept \; }
+}
+
+
+function delete_nft_firewall_table () {
+
+	#MAKE SURE CHAIN IS EMPTY!
+
+	nft delete chain inet secure_port_guard INPUT
+	nft delete table inet secure_port_guard
+}
+
+
+
+
+
+function get_external_network_interface() {
+	echo $(ip route get 8.8.8.8 | awk '{print $5}')
+}
+
 
 function main () {
 
+	#local TEST_TYPE=open_session
 	local TEST_TYPE=close_session
 
-	#if [ "$PAM_TYPE" = "open_session" ]
-	if [ "$TEST_TYPE" = "open_session" ]
+	#if a session was open, grant access to port for all ip address of logged in users
+	if [ "$PAM_TYPE" = "open_session" ]
+	#if [ "$TEST_TYPE" = "open_session" ]
 	then
 	    	echo "Open Session $(hostname -I)"
 	       
@@ -94,21 +166,17 @@ function main () {
 		w | awk 'NR > 2 {print $2}'  | while read ipaddress
 		do
 			allow_port_access_for_ipaddress $ipaddress $TESTPORT
-				
+									
 			#we can leave the loop here, because each new connection calls this function. hence only one new ipaddress at a time
-			break
+			#break
 		done
 	    
-	elif [ "$TEST_TYPE" = "close_session" ]; then
-	#elif [ "$PAM_TYPE" = "close_session" ]; then
+	#elif [ "$TEST_TYPE" = "close_session" ]; then
+	
+	#if a session was closed, remove all ipaddress from port access that are not logged in
+	elif [ "$PAM_TYPE" = "close_session" ]; then
 	    #echo "Closed Session $(hostname -I)"
 	    
-	    	#iterate through the ip addresses of the logged in users and add them to an array
-	    	who | awk '{print $5}' | tr -d '()' | while read ipaddress
-		do
-	    		add_ipaddress_to_array_of_logged_in_unique_ip_addresses $ipaddress
-	    		printf "logged in user with ipaddress: %s\n" $ipaddress
-	    	done
 	    	
 	    	#iterate through the ip addresses in the firewall, that have port access, hence all logged in ip adresses of the last script call
 	    	nft -a list table inet secure_port_guard | while read line
@@ -139,6 +207,7 @@ function main () {
 	    			if [ "$ipaddress_in_firewall_belongs_to_logged_in_user" = "FALSE" ]
 				then
 					remove_port_access_for_ipaddress $ipaddress_in_firewall $TESTPORT
+					
 				fi
 	    			
 	    		fi
@@ -146,61 +215,6 @@ function main () {
 	fi
 
 } 
-
-
-function port_process_is_docker () {
-
-	#teste, ob ein docker-container auf dem port lauscht
-	if ss -tap | grep $TESTPORT | grep docker
-	then
-		configure_firewall_for_docker_container
-	else
-		configure_firewall
-	fi
-
-}
-
-#extrahiere den process, der auf dem port lauscht
-#process=ss -tap | grep $TESTPORT | awk '{print $6}' | sed -n 's/.*"\([^"]*\)".*/\1/p'
-
-function configure_firewall_with_nft () {
-	return 0
-}
-
-function setup_nftables () {
-
-#block all other for port
-nft add rule inet secure_port_guard INPUT tcp dport $TESTPORT drop
-
-}
-
-
-
-#file that holds the ip_addresses of the previous iteration of this script, hence the logged in user at that time
-IP_ADDRESSES_PAST_CYCLE_STORAGE_FILE="logged_in_users.txt"
-
-#array_that holds the ip_addresses of the previous iteration of this script, hence the logged in user at that time
-ip_addresses_past_cycle=()
-
-#this function reads in the ip-addresses of the past cycle of this script from a text-file, 
-#hence the logged in user at that time
-function read_in_ip_addresses_past_cycle () {
-
-	if [ ! -e $IP_ADDRESSES_PAST_CYCLE_STORAGE_FILE ]
-	then
-		touch $IP_ADDRESSES_PAST_CYCLE_STORAGE_FILE
-	fi
-	
-	
-	mapfile -n 0 ZEILEN< "$IP_ADDRESSES_PAST_CYCLE_STORAGE_FILE"
-	local array_length=$(echo ${#ZEILEN[*]})
-	
-	
-	for (( i=0 ; i<$array_length; i++))
-	do
-		ip_addresses_past_cycle+=(${#ZEILEN[$i]})
-	done
-}
 
 
 
